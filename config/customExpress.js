32935cfdd254140ce62211b9695542579f78d7bc
Galdir Reges <galdir@gmail.com>
2020-10-22 18:29:50 -0300

    recebendo dados de formulario e de json

--- config/customExpress.js
+++ config/customExpress.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
 
 module.exports = () => {
     const app = express();
+
+    app.use(bodyParser.urlencoded({extended: true}))  // metodo do express recebendo de formulario (urlencoded)
+    app.use(bodyParser.json())
 
 consign()
     .include('controllers')
--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,5 +1,8 @@
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('voce esta na rota de atendimentos e esta realizando um get'));
 
-    app.post('/atendimentos',(req, res) => res.send('voce esta na rota de atendimentos e esta realiando um post'))
+    app.post('/atendimentos',(req, res) => {
+        console.log(req.body);
+        res.send('voce esta na rota de atendimentos e esta realiando um post');
+    });
 }

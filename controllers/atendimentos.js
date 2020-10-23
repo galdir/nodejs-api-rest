4639799dd0b004e8b60d072c4484d9f02461942c
Galdir Reges <galdir@gmail.com>
2020-10-22 21:16:52 -0300

    salvando atendimentos

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,8 +1,14 @@
+const Atendimento = require('../models/atendimentos')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('voce esta na rota de atendimentos e esta realizando um get'));
 
     app.post('/atendimentos',(req, res) => {
-        console.log(req.body);
-        res.send('voce esta na rota de atendimentos e esta realiando um post');
+        const atendimento = req.body;
+
+        Atendimento.adiciona(atendimento);
+        //console.log('atendimento enviado')
+        //console.log(req.body);
+        res.send('voce esta na rota de atendimentos e esta realizando um post');
     });
 }

02361990df4c157dbf9a12eed6baff3e882101c8
Galdir Reges <galdir@gmail.com>
2022-05-01 09:33:03 -0300

    clean

--- config/customExpress.js
+++ config/customExpress.js
@@ -2,15 +2,17 @@ const express = require('express');
 const consign = require('consign');
 const bodyParser = require('body-parser');
 
+
 module.exports = () => {
     const app = express();
 
-    app.use(bodyParser.urlencoded({extended: true}))  // metodo do express recebendo de formulario (urlencoded)
+    app.use(bodyParser.urlencoded({ extended: true }))  // metodo do express recebendo de formulario (urlencoded)
     app.use(bodyParser.json())
+    
 
-consign()
-    .include('controllers')
-    .into(app);
+    consign()
+        .include('controllers')
+        .into(app);
 
     return app;
 }
